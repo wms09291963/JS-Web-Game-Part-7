4987dbfa7e7519696694572511d46c17ab7361e0
Smith <william.smith@bankerslife.com>
2022-06-09 18:09:28 -0400

    assignment parameters

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+//npc.walkEast(2000)
+//async function moveNPC(){
+ //   await npc.walkEast(2000)
+ //   npc.walkSouth()
+//}
+async function moveNPC(){
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+}
+
+moveNPC()
 
 // Create the inventory
 const inventory = newInventory()
